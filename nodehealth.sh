2d46c383bfa7f0f7525a1a69d9fd89ab05e23629
Ubuntu <ubuntu@ip-172-31-39-178.eu-north-1.compute.internal>
2025-05-10 12:41:05 +0000

    this is my changes

---
+++ nodehealth.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+####################
+
+#author VIkash
+
+#date
+
+#script lists health output
+
+#############
+
+set -x #debug mode
+echo "print the disk space"
+
+df -h
+
+echo "print the memory"
+
+free -g
+
+echo "print the CPU"
+####
+uwegajdgjh
+
+set -e
+
+set -o
+
+set -o
+nproc
+
